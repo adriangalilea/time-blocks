603674f3d1906ecf8cebbbb52eb308c301771ca5
btmask <90320947+adriangalilea@users.noreply.github.com>
2023-06-29 21:04:51 +0200

    feat: web implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,140 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { Grid, Box, Flex, GridItem } from '@chakra-ui/react'
+import { motion } from "framer-motion"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+interface TimeBlockProps {
+  isHighlighted: boolean;
+  isStart: boolean;
+  isEnd: boolean;
 }
 
-export default App;
+// Custom hook to get and update the current time
+const useCurrentTime = () => {
+  const [time, setTime] = useState(new Date())
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date())
+    }, 5000)
+
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
+
+  return time
+}
+
+
+// Component to render a single grid item
+const TimeBlock: React.FC<TimeBlockProps & { delay: number }> = ({
+  isHighlighted,
+  isStart,
+  isEnd,
+  delay,
+}) => (
+  <motion.div
+    initial={{ background: '#1A1B2D' }}
+    animate={{ background: isHighlighted ? '#E5F901' : '#1A1B2D' }}
+    transition={{ delay, duration: 1 }} // Adjust duration here
+    style={{
+      height: '100%',
+      width: '100%',
+      border: '1px solid grey',
+      flexGrow: 1,
+      borderRadius: isStart ? '80px 0 0 80px' : isEnd ? '0 80px 80px 0' : '0',
+    }}
+  />
+);
+
+
+interface GridQuarterProps {
+  currentTime: Date;
+  isHour: boolean;
+  quarter: number;
+}
+
+
+const GridQuarter: React.FC<GridQuarterProps> = ({ currentTime, isHour, quarter }) => {
+  const items = [];
+
+  for (let i = 0; i < 3; i++) {
+    let isHighlighted;
+    if (isHour) {
+      isHighlighted = (currentTime.getHours() % 12) >= i + quarter * 3;
+    } else {
+      isHighlighted = Math.floor(currentTime.getMinutes() / 5) >= i + quarter * 3;
+    }
+
+    const isStart = quarter === 0 && i === 0;
+    const isEnd = quarter === 3 && i === 2;
+
+    items.push(
+      <TimeBlock
+        key={i}
+        isHighlighted={isHighlighted}
+        isStart={isStart}
+        isEnd={isEnd}
+        delay={(quarter * 3 + i) * 0.1} // Adjust delay here
+      />
+    );
+  }
+
+  return (
+    <div
+      style={{ display: 'flex', flexDirection: 'row', height: '100%', width: '25%', marginRight: quarter < 3 ? '2%' : '0' }}
+    >
+      {items}
+    </div>
+  )
+}
+
+interface GridColumnProps {
+  currentTime: Date;
+  isHour?: boolean;
+}
+
+const GridColumn: React.FC<GridColumnProps> = ({ currentTime, isHour }) => {
+  const quarters = []
+
+  for (let i = 0; i < 4; i++) {
+    quarters.push(
+      <GridQuarter
+        key={i}
+        currentTime={currentTime}
+        isHour={isHour || false}
+        quarter={i}
+      />
+    )
+  }
+
+  return (
+    <div
+      style={{ display: 'flex', flexDirection: 'row', height: '100%', width: '80vw' }}
+    >
+      {quarters}
+    </div>
+  )
+}
+
+export const App = () => {
+  const currentTime = useCurrentTime()
+
+  return (
+    <Flex justifyContent="center" alignItems="center" bg="background" h="100vh" w="100vw" p={48}>
+      <Grid
+        templateRows="repeat(2, 1fr)"
+        gap={44}
+        height="100%"
+      >
+        <GridColumn currentTime={currentTime} isHour />
+        <GridColumn currentTime={currentTime} />
+      </Grid>
+    </Flex>
+  )
+}
+
+
+
+export default App
---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+// theme.ts
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    light: '#E5F901',
+    dark: '#1A1B2D',
+    background: '#5858C1',
+  },
+})
